1a922476ed8085c0ea846dc4373db4d9e6bf3e3c
Dan Kang <dan@dskang.com>
2012-05-19 16:11:44 -0400

    Replace index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Agora::Application.routes.draw do
+  get "welcome/index"
+
   resources :guides
 
   resources :tours
@@ -52,7 +54,7 @@ Agora::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
