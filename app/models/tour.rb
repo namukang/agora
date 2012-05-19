73c3e984e226459fa80edec692ef7daa2764a6b5
Dan Kang <dan@dskang.com>
2012-05-19 17:47:08 -0400

    Added tours

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,4 +1,4 @@
 class Tour < ActiveRecord::Base
   attr_accessible :description, :guide_id, :title, :image
-  has_attached_file :image, :styles => { :medium => "360x270>", :thumb => "160x120>" }
+  has_attached_file :image
 end
