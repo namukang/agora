26cca7e52847ae08eb993d52a7e4555908d93d3c
Dan Kang <dan@dskang.com>
2012-05-20 17:43:47 -0400

    Change topic and times

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,5 @@
 class Tour < ActiveRecord::Base
-  attr_accessible :description, :guide_id, :title, :image, :banner, :price, :hours
+  attr_accessible :description, :guide_id, :title, :image, :banner, :price, :hours, :topic_list, :time_list
   has_attached_file :image
   has_attached_file :banner
   acts_as_taggable_on :topics, :times
