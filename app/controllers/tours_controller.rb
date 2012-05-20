c0a695bbd955e4ea959f9d5eef614873582b8773
Dan Kang <dan@dskang.com>
2012-05-19 23:30:27 -0400

    Make show tour look nice

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -18,6 +18,15 @@ class ToursController < ApplicationController
   # GET /tours/1.json
   def show
     @tour = Tour.find(params[:id])
+    if @tour.title == "Food Cart"
+      @guide = "Neal Donnelly"
+      @guide_image = "neal.png"
+      @guide_bio = "I'm a Princeton University student hailing from Northampton, MA (in western mass, not near Boston). I'm studying electrical engineering, I've worked three summers as a web developer, and I've dabbled in design, flash game development, and painting. I'm also an expert in New York's food carts, because I absolutely love food."
+    else
+      @guide = "Dan Kang"
+      @guide_image = "dk.png"
+      @guide_bio = "I am an ex-convict who knows my share of New York. I've dabbled in graffi and have many friends who are well-recognized in the New York graffiti scene."
+    end
 
     respond_to do |format|
       format.html # show.html.erb
